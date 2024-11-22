e7e24662cb2ff284b78c7dabb4facf670f506c41
Cute Puppy Dduo <164348579+Dddddduo@users.noreply.github.com>
2024-11-22 16:30:26 +0800

    题目提交信息成功发往后端的另一套接口

--- src/main/java/com/dduo/dduoj/judge/codesandbox/CodeSandboxFactory.java
+++ src/main/java/com/dduo/dduoj/judge/codesandbox/CodeSandboxFactory.java
@@ -15,10 +15,13 @@ public class CodeSandboxFactory {
      * */
     public static CodeSandbox NewInstance(String type) {
         switch (type) {
+            // 示例代码沙箱
             case "example":
                 return new ExampleCodeSandbox();
+            // 远程代码沙箱
             case "remote":
                 return new RemoteCodeSandbox();
+            // 第三方代码沙箱
             case "thirdParty":
                 return new ThirdPartyCodeSandbox();
             default:
